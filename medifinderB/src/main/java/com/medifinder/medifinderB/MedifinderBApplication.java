37d909f613efb7084f0ff501e904196a5ab94f99
Akshad gujarkar <akshadgujarkar12@gmail.com>
2025-02-26 12:13:36 +0530

    initial commit

---
+++ medifinderB/src/main/java/com/medifinder/medifinderB/MedifinderBApplication.java
@@ -0,0 +1,13 @@
+package com.medifinder.medifinderB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedifinderBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedifinderBApplication.class, args);
+	}
+
+}
---
+++ medifinderB/src/test/java/com/medifinder/medifinderB/MedifinderBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medifinder.medifinderB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedifinderBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
